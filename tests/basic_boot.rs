712d0d6aaeee06dfb15012be0c9542fce400e759
aleksandr <zaturskyi.aleksandr@gmail.com>
2022-05-13 17:56:36 +0300

    setup integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,31 +4,26 @@
 #![test_runner(rs_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello, world{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rs_os::test_panic_handler(info)
+}
+
+#[cfg(test)]
+mod test {
+    use rs_os::println;
+
+    #[test_case]
+    fn test_println() {
+        println!("test_println output");
+    }
 }
